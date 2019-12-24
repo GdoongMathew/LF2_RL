8d0e5af0087c20d37cc8d4dd195088ccbb1ce826
Gdoong426 <dungdudu426@gmail.com>
2019-12-25 01:51:51 +0800

    1. Import original lf2_rl model.

--- lf2_rl/Model.py
+++ lf2_rl/Model.py
@@ -2,28 +2,147 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as Func
 import numpy as np
+import gym
+import lf2_gym
 import cv2
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 
-class DQN(nn.Module):
-    def __init(self, action_n, state_n):
+class Net(nn.Module):
+    def __init__(self, action_n, state_n):
         super(DQN, self).__init__()
 
         picture_n, feature_n = state_n[0], state_n[1]
-        picture_n = cv2.resize(picture_n, [250, 498])
-        # input 4 x 250 x 498
-        input_ch = np.shape(picture_n)[-1]
-        self.conv1 = nn.Conv2d(input_ch, 32, kernel_size=3, stride=5).to(device)  # 32 x 50 x 100
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=4, stride=2).to(device)        # 64 x 25 x 50
+
+        # input 4 x 240 x 500
+        # 492, 232
+        self.conv1 = nn.Conv2d(4, 32, kernel_size=8, stride=4, dilation=2).to(device)  # 32 x 59 x 124
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=4, stride=5).to(device)        # 64 x 12 x 25
         self.conv3 = nn.Conv2d(64, 64, kernel_size=3, stride=4).to(device)        # 64 x
+
+        self.fc1 = nn.Linear(64 * 7 * 18, 200).to(device)
+        self.fc1.weight.data.normal_(0, 0.1)  # initialization
+        self.fc2 = nn.Linear(200 + feature_n, 50).to(device)
+        self.fc2.weight.data.normal_(0, 0.1)  # initialization
+        self.out = nn.Linear(50, action_n).to(device)
+        self.out.weight.data.normal_(0, 0.1)  # initialization
 
     def forward(self, x):
         img, feature = x[0], x[1]
-        img = cv2.resize(img, [250, 498])
+        # img = cv2.resize(img, (240, 500))
 
         img = Func.relu(self.conv1(img))
         img = Func.relu(self.conv2(img))
         img = Func.relu(self.conv3(img))
-        img.view()
+        img = img.view(img.size(0), -1)
+        img = Func.relu(self.fc1(img))
+
+        # feature nn
+        x = torch.cat((img, feature), 1)
+        x = Func.relu(self.fc2(x))
+        return self.out(x)
+
+
+class DQN:
+    def __init__(self, action_n, state_n, env_shape, learning_rate=0.01, reward_decay=0.9, epsilon=0.5,
+                 memory_capacity=2000, batch_size=32, update_freq=100):
+        self.eval_net = Net(action_n, state_n)
+        self.target_net = Net(action_n, state_n)
+        self.action_n = action_n
+        self.state_n = state_n
+        self.lr = learning_rate
+        self.gamma = reward_decay
+        self.env_shape = env_shape
+        self.epsilon = epsilon
+        self.memory_capacity = memory_capacity
+        self.batch_size = batch_size
+        self.update_freq = update_freq
+
+        self.step_counter = 0
+        self.memory_counter = 0
+        self.memory = np.zeros((self.memory_capacity, (4 * self.state_n[0][0] * self.state_n[0][1] + self.state_n[1]) * 2 + 2))
+        self.optimizer = torch.optim.Adam(self.eval_net.parameters(), lr=self.lr)
+        self.loss_func = nn.MSELoss()
+
+    def choose_action(self, x):
+        img = torch.unsqueeze(torch.FloatTensor(x[0]).to(device), 0)
+        feature = torch.unsqueeze(torch.FloatTensor(x[1]).to(device), 0)
+        x = [img, feature]
+
+        if np.random.uniform() < self.epsilon:
+            action_val = self.eval_net.forward(x).gpu()
+            action = torch.max(action_val, 1)[1].data.numpy()
+            action = action[0]
+
+        else:
+            action = np.random.randint(0, self.action_n)
+            action = action if self.env_shape == 0 else action.reshape(self.env_shape)
+
+        return action
+
+    def store_transition(self, s, a, r, s_):
+        s = np.append(np.reshape(s[0], -1), s[1])  # [(4, 160, 380), 28] -> 4 * 160 * 380 + 28
+        s_ = np.append(np.reshape(s_[0], -1), s_[1])
+        transition = np.hstack((s, [a, r], s_))
+        # replace the old memory with new memory
+        index = self.memory_counter % self.memory_capacity
+        self.memory[index, :] = transition
+        self.memory_counter += 1
+
+    def learn(self):
+        # target parameter update
+        if self.step_counter % self.update_freq == 0:
+            self.target_net.load_state_dict(self.eval_net.state_dict())
+        self.step_counter += 1
+
+        # sample batch transitions
+        sample_index = np.random.choice(self.memory_capacity, self.batch_size)
+        b_memory = self.memory[sample_index, :]
+        picture_idx = 4 * self.state_n[0][0] * self.state_n[0][1]
+        feature_idx = self.state_n[1]
+        state_idx = picture_idx + feature_idx  # 4 * 160 * 380 + 28
+        b_picture = torch.FloatTensor(b_memory[:, :picture_idx]).to(device)
+        b_feature = torch.FloatTensor(b_memory[:, picture_idx:state_idx]).to(device)
+        b_a = torch.LongTensor(b_memory[:, state_idx:state_idx + 1].astype(int)).to(device)
+        b_r = torch.FloatTensor(b_memory[:, state_idx + 1:state_idx + 2]).to(device)
+        b_picture_ = torch.FloatTensor(b_memory[:, -state_idx:-feature_idx]).to(device)
+        b_feature_ = torch.FloatTensor(b_memory[:, -feature_idx:]).to(device)
+        # reshape (batch_size, 4, 160, 380)
+        b_picture = np.reshape(b_picture, (self.batch_size, 4, self.state_n[0][0], self.state_n[0][1])).to(device)
+        b_picture_ = np.reshape(b_picture_, (self.batch_size, 4, self.state_n[0][0], self.state_n[0][1])).to(device)
+        b_s = [b_picture, b_feature]
+        b_s_ = [b_picture_, b_feature_]
+
+        # q_eval w.r.t the action in experience
+        q_eval = self.eval_net(b_s).gather(1, b_a)  # shape (batch, 1)
+        q_next = self.target_net(b_s_).detach()  # detach from graph, don't backpropagate
+        q_target = b_r + self.gamma * q_next.max(1)[0].view(self.batch_size, 1)  # shape (batch, 1)
+        loss = self.loss_func(q_eval, q_target)
+
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+
+    def save_model(self):
+        torch.save(self.eval_net.state_dict(),
+                   'model/DQN/mix_eval_{}_{}_{}.pkl'.format(self.lr, self.epsilon, self.batch_size))
+        torch.save(self.target_net.state_dict(),
+                   'model/DQN/mix_target_{}_{}_{}.pkl'.format(self.lr, self.epsilon, self.batch_size))
+
+    def load_model(self, model_name):
+        self.eval_net.load_state_dict(torch.load(model_name))
+
+
+
+if __name__ == '__main__':
+    karg = dict(frame_stack=4, frame_skip=1, reset_skip_sec=2, mode='mix')
+    lf2_env = gym.make('LittleFighter2-v0', **karg)
+
+    act = lf2_env.get_action_space()
+    obs = lf2_env.reset()
+
+    obs = [obs['Game_Screen'], obs['Info']]
+
+    dqn = DQN(act, obs)
+    dqn.forward(obs)
