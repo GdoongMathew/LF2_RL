fd0af38739e3e8597784b867ee72a34b49154b8f
Gdoong426 <dungdudu426@gmail.com>
2020-05-12 00:57:03 +0800

    1. add prepare_dataset script.

--- lf2_dataset/prepare_dataset.py
+++ lf2_dataset/prepare_dataset.py
@@ -1,7 +1,14 @@
 import gym
 import lf2_gym
+import cv2
+
+class DataCapture:
+    def __init__(self, path):
+        self.path = path
+
+    def action_output(self, basic_action=False):
+        if basic_action:
 
 mode = 'mix'
 karg = dict(frame_stack=3, frame_skip=1, reset_skip_sec=2, mode=mode)
 lf2_env = gym.make('LittleFighter2-v0', **karg)
-
